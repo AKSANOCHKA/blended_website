de26c5df2c6c3f3bc917f79193024db697dfee9e
Oksana Skakun <ksanas180882@gmail.com>
2025-07-30 13:03:03 +0300

    add all

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,1 +1,7 @@
 //Логіка сторінки Wishlist
+import { themeToggle } from './js/helpers.js';//Логіка сторінки Wishlist
+import { changeTheme } from './js/storage.js';
+
+
+changeTheme();
+themeToggle();
