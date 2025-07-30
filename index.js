0c5489914f7eb6e8d06e4ce035ac952290976b10
AKSANOCHKA <ksanas180882@gmail.com>
2025-07-30 10:03:49 +0000

    Deploying to gh-pages from @ AKSANOCHKA/blended_website@de26c5df2c6c3f3bc917f79193024db697dfee9e 🚀

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import"./assets/styles-JE8YjOlG.js";import{a}from"./assets/vendor-N5iQpiFS.js";async function _(){try{return(await a("https://dummyjson.com/products/category-list")).data}catch(t){throw console.log(t.message),new Error("Something went wrong!")}}async function l(t=1){try{return(await a.get(`https://dummyjson.com/products?limit=12&skip=${(t-1)*12}`)).data}catch(e){throw console.log(e.message),new Error("Something went wrong!")}}async function h(t,e=1){try{return(await a.get(`https://dummyjson.com/products/category/${t}?limit=12&skip=${(e-1)*12}`)).data}catch(o){throw console.log(o.message),new Error("Something went wrong!")}}async function y(t){try{const o=(await a.get(`https://dummyjson.com/products/${t}`)).data;return console.log("Loading product:",t),o}catch(e){console.log(e)}}const c={categotiesList:document.querySelector(".categories"),productsList:document.querySelector(".products"),divNotFound:document.querySelector(".not-found"),themeToggleBtn:document.querySelector(".theme-toggle-btn"),modalProduct:document.querySelector(".modal"),modalProductContent:document.querySelector(".modal-product"),closeModalBtn:document.querySelector(".modal__close-btn")};function f(t){const e=["All",...t].map(o=>`
+import"./assets/styles-JE8YjOlG.js";import{a as n}from"./assets/vendor-N5iQpiFS.js";import{r as c,c as _,t as y,o as f,a as l}from"./assets/storage-CPDqhayU.js";async function h(){try{return(await n("https://dummyjson.com/products/category-list")).data}catch(t){throw console.log(t.message),new Error("Something went wrong!")}}async function p(t=1){try{return(await n.get(`https://dummyjson.com/products?limit=12&skip=${(t-1)*12}`)).data}catch(o){throw console.log(o.message),new Error("Something went wrong!")}}async function L(t,o=1){try{return(await n.get(`https://dummyjson.com/products/category/${t}?limit=12&skip=${(o-1)*12}`)).data}catch(s){throw console.log(s.message),new Error("Something went wrong!")}}async function $(t){try{const s=(await n.get(`https://dummyjson.com/products/${t}`)).data;return console.log("Loading product:",t),s}catch(o){console.log(o)}}function w(t){const o=["All",...t].map(s=>`
     <li class="categories__item">
-        <button class="categories__btn" type="button">${o}</button>
+        <button class="categories__btn" type="button">${s}</button>
     </li>
- `).join("");c.categotiesList.insertAdjacentHTML("beforeend",e)}function u(t){const e=t.map(({id:o,title:s,thumbnail:n,brand:r,category:d,price:i})=>`
-    <li class="products__item" data-id="${o}">
-    <img class="products__image" src="${n}" alt="${s}"/>
-    <p class="products__title">${s}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:${r}</span></p>
-    <p class="products__category">Category:${d}</p>
-    <p class="products__price">Price:${i}$</p>
+ `).join("");c.categotiesList.insertAdjacentHTML("beforeend",o)}function u(t){const o=t.map(({id:s,title:e,thumbnail:r,brand:a,category:i,price:d})=>`
+    <li class="products__item" data-id="${s}">
+    <img class="products__image" src="${r}" alt="${e}"/>
+    <p class="products__title">${e}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${a}</span></p>
+    <p class="products__category">Category:${i}</p>
+    <p class="products__price">Price:${d}$</p>
  </li>
-    `).join("");c.productsList.insertAdjacentHTML("beforeend",e)}function L(){c.divNotFound.classList.remove("not-found--visible"),c.productsList.innerHTML=""}async function w(t){if(t.target.classList.contains("categories__btn")){const e=t.target;c.categotiesList.querySelectorAll(".categories__btn").forEach(r=>{r.classList.remove("categories__btn--active")}),e.classList.add("categories__btn--active");const s=e.textContent,n=s==="All"?await l():await h(s);L(),n.products.length!==0?u(n.products):c.divNotFound.classList.add("not-found--visible")}}function $(){c.themeToggleBtn.addEventListener("click",()=>{const t=document.documentElement,o=t.dataset.theme==="dark"?"light":"dark";t.dataset.theme=o,localStorage.setItem("site-theme",o)})}function b(){c.modalProduct.classList.add("modal--is-open")}function m(){c.modalProduct.classList.remove("modal--is-open")}function P(){window.addEventListener("DOMContentLoaded",()=>{const t=localStorage.getItem("site-theme"),e=document.documentElement;e.dataset.theme=t||"light"})}function v(t){const{images:e,description:o,title:s,tags:n="Not specified",shippingInformation:r="Not specified",returnPolicy:d="Not specified",price:i}=t,p=`
-<img class="modal-product__img" src="${e[0]}" alt="${s}" />
+    `).join("");c.productsList.insertAdjacentHTML("beforeend",o)}function b(){c.divNotFound.classList.remove("not-found--visible"),c.productsList.innerHTML=""}async function P(t){if(t.target.classList.contains("categories__btn")){const o=t.target;c.categotiesList.querySelectorAll(".categories__btn").forEach(a=>{a.classList.remove("categories__btn--active")}),o.classList.add("categories__btn--active");const e=o.textContent,r=e==="All"?await p():await L(e);b(),r.products.length!==0?u(r.products):c.divNotFound.classList.add("not-found--visible")}}function v(t){const{images:o,description:s,title:e,tags:r="Not specified",shippingInformation:a="Not specified",returnPolicy:i="Not specified",price:d}=t,m=`
+<img class="modal-product__img" src="${o[0]}" alt="${e}" />
       <div class="modal-product__content">
-        <p class="modal-product__title">${s}</p>
+        <p class="modal-product__title">${e}</p>
         <ul class="modal-product__tags">
-    ${n.map(g=>`<li class="modal-product__tag">${g}</li>`).join("")}</ul>
-        <p class="modal-product__description">${o}</p>
-        <p class="modal-product__shipping-information">Shipping:${r}</p>
-        <p class="modal-product__return-policy">Return Policy:${d}</p>
-        <p class="modal-product__price">Price: ${i}$</p>
+    ${r.map(g=>`<li class="modal-product__tag">${g}</li>`).join("")}</ul>
+        <p class="modal-product__description">${s}</p>
+        <p class="modal-product__shipping-information">Shipping:${a}</p>
+        <p class="modal-product__return-policy">Return Policy:${i}</p>
+        <p class="modal-product__price">Price: ${d}$</p>
         <button class="modal-product__buy-btn" type="button">Buy</button>
       </div>
-`;console.log("Product received:",t),document.querySelector(".modal-product").innerHTML=p}async function S(){f(await _());const t=await l();u(t.products)}P();$();S();c.categotiesList.addEventListener("click",w);c.productsList.addEventListener("click",async t=>{const e=t.target.closest(".products__item");if(!e)return;const o=e.dataset.id;b();try{const s=await y(o);v(s)}catch(s){console.log(s)}});c.closeModalBtn.addEventListener("click",m);c.modalProduct.addEventListener("click",t=>{c.modalProductContent.contains(t.target)||m()});
+`;console.log("Product received:",t),document.querySelector(".modal-product").innerHTML=m}async function k(){w(await h());const t=await p();u(t.products)}_();y();k();c.categotiesList.addEventListener("click",P);c.productsList.addEventListener("click",async t=>{const o=t.target.closest(".products__item");if(!o)return;const s=o.dataset.id;f();try{const e=await $(s);v(e)}catch(e){console.log(e)}});c.closeModalBtn.addEventListener("click",l);c.modalProduct.addEventListener("click",t=>{c.modalProductContent.contains(t.target)||l()});
 //# sourceMappingURL=index.js.map
---
+++ wishlist.js
@@ -0,0 +1,2 @@
+import"./assets/styles-JE8YjOlG.js";import{c as e,t as m}from"./assets/storage-CPDqhayU.js";e();m();
+//# sourceMappingURL=wishlist.js.map
