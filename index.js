0e4f7554127ad59bf9ba4024fc757f2f3ec3a117
AKSANOCHKA <ksanas180882@gmail.com>
2025-07-30 10:30:08 +0000

    Deploying to gh-pages from @ AKSANOCHKA/blended_website@5a0ada10a5f64909969efc31bdc78722896fc7c1 🚀

--- index.js
+++ index.js
@@ -1,26 +1,2 @@
-import"./assets/styles-JE8YjOlG.js";import{a as n}from"./assets/vendor-N5iQpiFS.js";import{r as c,c as _,t as y,o as f,a as l}from"./assets/storage-CPDqhayU.js";async function h(){try{return(await n("https://dummyjson.com/products/category-list")).data}catch(t){throw console.log(t.message),new Error("Something went wrong!")}}async function p(t=1){try{return(await n.get(`https://dummyjson.com/products?limit=12&skip=${(t-1)*12}`)).data}catch(o){throw console.log(o.message),new Error("Something went wrong!")}}async function L(t,o=1){try{return(await n.get(`https://dummyjson.com/products/category/${t}?limit=12&skip=${(o-1)*12}`)).data}catch(s){throw console.log(s.message),new Error("Something went wrong!")}}async function $(t){try{const s=(await n.get(`https://dummyjson.com/products/${t}`)).data;return console.log("Loading product:",t),s}catch(o){console.log(o)}}function w(t){const o=["All",...t].map(s=>`
-    <li class="categories__item">
-        <button class="categories__btn" type="button">${s}</button>
-    </li>
- `).join("");c.categotiesList.insertAdjacentHTML("beforeend",o)}function u(t){const o=t.map(({id:s,title:e,thumbnail:r,brand:a,category:i,price:d})=>`
-    <li class="products__item" data-id="${s}">
-    <img class="products__image" src="${r}" alt="${e}"/>
-    <p class="products__title">${e}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand:${a}</span></p>
-    <p class="products__category">Category:${i}</p>
-    <p class="products__price">Price:${d}$</p>
- </li>
-    `).join("");c.productsList.insertAdjacentHTML("beforeend",o)}function b(){c.divNotFound.classList.remove("not-found--visible"),c.productsList.innerHTML=""}async function P(t){if(t.target.classList.contains("categories__btn")){const o=t.target;c.categotiesList.querySelectorAll(".categories__btn").forEach(a=>{a.classList.remove("categories__btn--active")}),o.classList.add("categories__btn--active");const e=o.textContent,r=e==="All"?await p():await L(e);b(),r.products.length!==0?u(r.products):c.divNotFound.classList.add("not-found--visible")}}function v(t){const{images:o,description:s,title:e,tags:r="Not specified",shippingInformation:a="Not specified",returnPolicy:i="Not specified",price:d}=t,m=`
-<img class="modal-product__img" src="${o[0]}" alt="${e}" />
-      <div class="modal-product__content">
-        <p class="modal-product__title">${e}</p>
-        <ul class="modal-product__tags">
-    ${r.map(g=>`<li class="modal-product__tag">${g}</li>`).join("")}</ul>
-        <p class="modal-product__description">${s}</p>
-        <p class="modal-product__shipping-information">Shipping:${a}</p>
-        <p class="modal-product__return-policy">Return Policy:${i}</p>
-        <p class="modal-product__price">Price: ${d}$</p>
-        <button class="modal-product__buy-btn" type="button">Buy</button>
-      </div>
-`;console.log("Product received:",t),document.querySelector(".modal-product").innerHTML=m}async function k(){w(await h());const t=await p();u(t.products)}_();y();k();c.categotiesList.addEventListener("click",P);c.productsList.addEventListener("click",async t=>{const o=t.target.closest(".products__item");if(!o)return;const s=o.dataset.id;f();try{const e=await $(s);v(e)}catch(e){console.log(e)}});c.closeModalBtn.addEventListener("click",l);c.modalProduct.addEventListener("click",t=>{c.modalProductContent.contains(t.target)||l()});
+import"./assets/styles-B-BVPyWf.js";import{i as e,a,s as i,b as d,r as s,k as r,h as n,c as o,l as c,m as l,d as m,e as C,n as L,o as h,p as u,j as g}from"./assets/handlers-CFJkzAPY.js";import"./assets/vendor-4yCzdkXl.js";e();a();i();d();async function k(){L(await h());const t=await u();g(t.products)}k();s.categotiesList.addEventListener("click",r);s.productsList.addEventListener("click",n);s.modalCloseBtn.addEventListener("click",o);s.searchForm.addEventListener("submit",c);s.formBtnClear.addEventListener("click",l);s.addCartBtn.addEventListener("click",m);s.addWishListBtn.addEventListener("click",C);
 //# sourceMappingURL=index.js.map
